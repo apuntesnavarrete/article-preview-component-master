0071adfb427217609d9f552fdaf839a60a23d553
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-05-27 21:41:41 -0500

    evento funcionando

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-alert("oli")
+let share = document.getElementsByClassName("footer_2")[0]
+console.log(share)
+
+document.addEventListener("click",(e)=>{
+    if(e.target.matches(".share")){
+        console.log("este click solo debe ver aqui")
+        share.style.display = "flex";
+    }
+})
