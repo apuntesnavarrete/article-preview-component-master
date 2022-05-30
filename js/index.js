132fd0aa03056cda3a13cd05cfa2f8230f5f87ed
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-05-30 00:40:51 -0500

    version 1.0 terminado

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-let share = document.getElementsByClassName("footer_2")[0]
-console.log(share)
+let share = document.getElementsByClassName("footer_2")[0];
+console.log(share);
 
-document.addEventListener("click",(e)=>{
-    if(e.target.matches(".share")){
-        console.log("este click solo debe ver aqui")
-        share.style.display = "flex";
-    }
-})
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".share")) {
+    console.log("este click solo debe ver aqui");
+    share.style.display = "flex";
+  }
+});
